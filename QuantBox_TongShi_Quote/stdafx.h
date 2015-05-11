050f703ad868838a280938a42418c42085617dcc
伍侃 <wu-kan@163.com>
2015-05-11 13:46:42 +0800

    输出路径调整

--- QuantBox_TongShi_Quote/stdafx.h
+++ QuantBox_TongShi_Quote/stdafx.h
@@ -4,14 +4,10 @@
 
 #pragma once
 
-<<<<<<< HEAD
-#if defined _WIN32 || WIN32 || _WINDOWS
-=======
 #ifndef VC_EXTRALEAN
 #define VC_EXTRALEAN            // Exclude rarely-used stuff from Windows headers
 #endif
 
->>>>>>> 054867c02a55335216690b5d3b33cf9e753fd1aa
 #include "targetver.h"
 
 #define _ATL_CSTRING_EXPLICIT_CONSTRUCTORS      // some CString constructors will be explicit
@@ -33,9 +29,6 @@
 #include <afxdao.h>                     // MFC DAO database classes
 #endif // _AFX_NO_DAO_SUPPORT
 
-<<<<<<< HEAD
-#else
-=======
 #ifndef _AFX_NO_OLE_SUPPORT
 #include <afxdtctl.h>           // MFC support for Internet Explorer 4 Common Controls
 #endif
@@ -43,7 +36,3 @@
 #include <afxcmn.h>                     // MFC support for Windows Common Controls
 #endif // _AFX_NO_AFXCMN_SUPPORT
 #include <afxcontrolbars.h>
->>>>>>> 054867c02a55335216690b5d3b33cf9e753fd1aa
-
-#endif // defined
-
